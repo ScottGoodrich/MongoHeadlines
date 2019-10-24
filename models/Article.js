bc3a8349f8fab5160602acc9102fae76338e2ccc
Scott Goodrich <scottgoodrich516@gmail.com>
2019-10-24 16:27:22 -0700

    initial commit

---
+++ models/Article.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  // `comment` is an object that stores a Comment id
+  // The ref property links the ObjectId to the Comment model
+  // This allows us to populate the Article with an associated Comment
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
